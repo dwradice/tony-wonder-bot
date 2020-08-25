827aa6b8d1e619d5b32d6c821f220430b4e1b79b
dwradice <dwradic@gmail.com>
2020-08-25 16:25:44 -0700

    promise

--- bot.js
+++ bot.js
@@ -9,21 +9,25 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  const gifs = [
-    'https://i.pinimg.com/originals/8c/c5/40/8cc5401d447bd7c5485e3e709a7c7e22.gif',
-    'https://y.yarn.co/574e1b36-1f10-4d7d-bdf7-20aa2f367da6_text_hi.gif',
-    'https://i.imgur.com/5yMlrFZ.gif',
-  ];
+  try {
+    const gifs = [
+      'https://i.pinimg.com/originals/8c/c5/40/8cc5401d447bd7c5485e3e709a7c7e22.gif',
+      'https://y.yarn.co/574e1b36-1f10-4d7d-bdf7-20aa2f367da6_text_hi.gif',
+      'https://i.imgur.com/5yMlrFZ.gif',
+    ];
 
-  if (message.content.match(/wonder/i) && !message.author.bot) {
-    message.channel.send('Did somebody say "Wonder?"', {
-      files: [
-        {
-          attachment: gifs[Math.floor(Math.random() * gifs.length)],
-          name: 'tony-wonder.gif',
-        },
-      ],
-    });
+    if (message.content.match(/wonder/i) && !message.author.bot) {
+      message.channel.send('Did somebody say "Wonder?"', {
+        files: [
+          {
+            attachment: gifs[Math.floor(Math.random() * gifs.length)],
+            name: 'tony-wonder.gif',
+          },
+        ],
+      });
+    }
+  } catch (err) {
+    console.log('Error');
   }
 });
 
